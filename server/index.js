bcc3c0957d70a82c03e67715d9f7292302ec1c0f
jimmylu1 <jimmy88lu@gmail.com>
2019-09-23 16:19:23 -0700

    able to get data from api end points

--- server/index.js
+++ server/index.js
@@ -10,3 +10,30 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//Search for movie by movie title
+app.get(`/movies/:movieTitle`, async (req, res) => {
+  console.log(req.params.movieTitle);
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${req.params.movieTitle}&page=1&include_adult=true`
+    );
+    //If successful, send data
+    res.status(200).send({ data: response.data });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//Get top 20 trending movies for the week
+app.get("/popular", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/week?api_key=${key}`
+    );
+    //If successful, send data
+    res.status(200).send({ data: response.data });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
